31f08485534bcf115d85f5b110641a4679ceee1b
nadeem <nadeemahmad199726@gmail.com>
2025-09-11 14:02:22 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
     origin: ['https://supplysyncapp.netlify.app', 'http://localhost:5174'],
+     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   }),
 )
