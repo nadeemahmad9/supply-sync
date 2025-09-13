e355e5d99e41ee1a23aaf8d5db9259eba2a1274e
nadeem <nadeemahmad199726@gmail.com>
2025-09-13 16:31:15 +0530

    passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -10,12 +10,19 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: process.env.NODE_ENV === "production"
+  ? "https://supply-sync.onrender.com/api/auth/google/callback"
+  : "http://localhost:5000/api/auth/google/callback",
+
+
     scope: ["profile", "email"],
 
+
     },
+    
     async (accessToken, refreshToken, profile, done) => {
       try {
+
                 console.log("Google Profile:", profile); // ✅ Debug
 
         const email = profile.emails?.[0]?.value;
